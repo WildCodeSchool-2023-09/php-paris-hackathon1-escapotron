06564a01f17114ed2183af53d7a098c27fb864d0
nicolasalibert <contact@nicolasalibert.me>
2023-11-24 10:02:44 +0100

    samarsh

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 class PageController extends AbstractController
 {
+    
+
     /**
      * Display home page
      */
@@ -21,20 +23,40 @@ class PageController extends AbstractController
     {
         return $this->twig->render('/infos.html.twig');
     }
-    public function generateur(): string
+    public function generateur(string $fete, string $gens, string $raison, string $ton): string
     {
-        // tableaux de valeurs //
+        // GENS //
 
-        $famille = ['mes chéris', 'la smallah', 'tout le monde',];
-        $conjoint = ['mon bébé', 'chouchou', 'ptit coeur',];
-        $amis = ['les potos', 'la bande', 'mes bestas', 'les amis'];
-        $collegues = ['l\'équipe', 'chers collègues', 'tout le monde'];
+        $famille = ['mes chéris',
+                    'la smallah',
+                    'tout le monde',
+                    ];
 
+        $conjoint = ['mon bébé',
+                    'chouchou',
+                    'ptit coeur',
+                    ];
+
+        $amis = ['les potos',
+                'la bande',
+                'mes bestas',
+                'les amis'
+                ];
+
+        $collegues = [ 'l\'équipe',
+                    'chers collègues',
+                    'tout le monde'
+                    ];
+
+
+        // RAISONS
 
         $travail = ['rendez-vous avec le président',
                     'un client qui a urgemment besoin de mes services',
                     'le quaterly back-up strategic outsourcing qui ne va pas se faire tout seul',
-                    'un mail primordial à écrire'];
+                    'un mail primordial à écrire',
+                    ];
+
         $absurde = ['rencontré un oiseau dans l\'espace',
                     'mon chien qui m\'a défié au strip poker',
                     'le roi de Suede qui vient réparer mon xylophone',
@@ -42,101 +64,106 @@ class PageController extends AbstractController
                     'volé la déclaration d\'indépendance des états-unis',
                     'ggrzuig grzuguzg rupvo',
                     'cours de yoyo',
-                    'mon chien qui a mangé ma voiture'];
+                    'mon chien qui a mangé ma voiture',
+                    ];
+
         $sante = [  'perdu mes dents',
                     'une grippe du pied',
                     'une fracture d\'un endroit que je garderai secret',
                     'attrapé la peste qui infecte mon village',
                     'une crise cardiaque',
-                    'un avc'];
+                    'un avc',
+                    ];
+
         $politique = [  'été enfermé dans un goulag',
                         'commencé une révolution au Machu Pichu',
                         'le KGB aux trousses',
-                        'été réquisitionné pour le dépouillement des élections'];
+                        'été réquisitionné pour le dépouillement des élections',
+                    ];
+
+
+        // TON
 
         $delicat = ['en espérant ne pas vous faire trop de peine',
                     'je vous présente mes plus sincères excuses',
                     'je vous prie de bien vouloir m\'excuser',
-                    'sachez que je regrette amèrement cette absence'];
+                    'sachez que je regrette amèrement cette absence',
+                ];
+
         $corpo = [  'cordialement',
                     'amicalement votre',
                     'professionnellement votre',
                     'bien à vous',
                     'dans l\'espoir que mes attentions trouverons écho à votre mansuétude,
-                    je vous souhtaite une productive soirée'];
+                    je vous souhtaite une productive soirée',
+                ];
+
         $neutre = [ 'encore désolé tout le monde',
                     'je suis vraiment confus',
-                    'ce sera pour la prochaine fois'];
+                    'ce sera pour la prochaine fois',
+                ];
+
         $mechant = ['et je ne vous aime pas',
                     'idiots',
                     'imbéciles',
-                    'et je vous invite à aller vous asseoir sur un cactus',];
+                    'et je vous invite à aller vous asseoir sur un cactus',
+                ];
 
-
-        //gens//
-
-        if ($_GET['gens'] === "famille") {
-            $gens = array_rand($famille);
+        if ($fete === "halloween") {
+            $excuse['fete'] = "Halloween";
         }
-
-        if ($_GET['gens'] === "conjoint") {
-            $gens = array_rand($conjoint);
+        else if ($fete === "noel") {
+            $excuse['fete'] = "Noël";
         }
-
-        if ($_GET['gens'] === "amis") {
-            $gens = array_rand($amis);
+        else if ($fete === "nouvel-an") {
+            $excuse['fete'] = "le Nouvel An";
         } else {
-            $gens = array_rand($collegues);
+            $excuse['fete'] = "Hanoucca";
         }
 
-        //fete : pas de tableau de valeur : le GET renvoie directement la fete choisie//
-        //excuse//
-        $absurde = [''];
-        $sante = [''];
-        $politique = [''];
+        // GENS
 
-        if ($_GET['excuse'] === "travail") {
-            $excuse = array_rand($travail);
+        if ($gens === "famille") {
+            $excuse['gens'] = "oui";
         }
-
-        if ($_GET['excuse'] === "absurde") {
-            $excuse = array_rand($absurde);
+        else if ($gens === "conjoint") {
+            $excuse['gens'] = $conjoint[array_rand($conjoint)];
         }
-
-        if ($_GET['excuse'] === "sante") {
-            $excuse = array_rand($sante);
+        else if ($gens === "amis") {
+            $excuse['gens'] =$amis[array_rand($amis)];
         } else {
-            $excuse = array_rand($politique);
+            $excuse['gens'] = $collegues[array_rand($collegues)];
         }
 
-        //ton//
-        $delicat = [''];
-        $corpo = [''];
-        $neutre = [''];
-        $mechant = [''];
+        // EXCUSE
 
-        if ($_GET['ton'] === "delicat") {
-            $ton = array_rand($delicat);
+        if ($raison === "travail") {
+            $excuse['raison'] = $travail[array_rand($travail)];
         }
-        if ($_GET['ton'] === "corpo") {
-            $ton = array_rand($corpo);
+        else if ($raison === "absurde") {
+            $excuse['raison'] = $absurde[array_rand($absurde)];
         }
-        if ($_GET['ton'] === "neutre") {
-            $ton = array_rand($neutre);
+        else if ($raison === "sante") {
+            $excuse['raison'] = $sante[array_rand($sante)];
         } else {
-            $ton = array_rand($mechant);
+            $excuse['raison'] = $politique[array_rand($politique)];
         }
 
-        $ton = [''];
+        if ($ton === "delicat") {
+            $excuse['ton'] = $delicat[array_rand($delicat)];
+        }
+        else if ($ton === "corpo") {
+            $excuse['ton'] = $corpo[array_rand($corpo)];
+        }
+        else if ($ton === "neutre") {
+            $excuse['ton'] = $neutre[array_rand($neutre)];
+        } else {
+            $excuse['ton'] = $mechant[array_rand($mechant)];
+        }
 
-        $excuseparfaite = "désolé " . $gens . " je ne peux pas venir à "
-                        . $_GET['fete'] . " car j'ai " . $excuse . ", " . $ton;
+        $excuseParfaite = "« Désolé " . $excuse['gens'] . " je ne peux pas venir pour "
+                        . $excuse['fete'] . " car j'ai " . $excuse['raison'] . ", " . $excuse['ton'] . ". »";
 
-        return $this->twig->render('finale.html.twig', ['excuseparfaite' => $excuseparfaite]);
-    }
-
-    public function result(): string
-    {
-        return $this->twig->render('/result.html.twig');
+        return $this->twig->render('result.html.twig', ['excuseParfaite' => $excuseParfaite]);
     }
 }
